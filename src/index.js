995d70289da721f2a1724360b3c0b18a40aa0659
Raihan-Sharif <raihan.raju@gmail.com>
2021-07-08 21:46:48 +0600

    functional component and class component practice

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import reactDom from 'react-dom';
+import ReactDom from 'react-dom';
 
 // const element = React.createElement('h1', { className: 'HeaderClass' }, 'Hello World!');
 const tabIndex = 0;
-const element = (
-    <h1 className="HeaderClass" tabIndex={tabIndex}>
-        <span> Hello World!</span>
-        <img src="" alt="Hello" />
-    </h1>
-);
+// Example: start- functional Component
+// function UserElement({ user, locale }) {
+//     return (
+//         <h1 className="HeaderClass" tabIndex={tabIndex}>
+//             <span> Hello {user}!</span>
+//             <h5>{new Date().toLocaleString(locale)}</h5>
+//             <img src="" alt="opps!" />
+//         </h1>
+//     );
+// }
+// Example: End - functional Component
+
+// Example: start- class Component
+class UserElement extends React.Component {
+    render() {
+        const { locale, children } = this.props;
+        return (
+            <div className="HeaderClass" tabIndex={tabIndex}>
+                <h2> Hello {children}!</h2>
+                <h5>{new Date().toLocaleString(locale)}</h5>
+                <img src="" alt="opps!" />
+            </div>
+        );
+    }
+}
+// Example: End - class Component
+
 /*
 {
   type:'h1',
@@ -33,6 +55,8 @@ const element = (
   }
 }
 */
-console.log(element);
+// console.log(element);
 
-reactDom.render(element, document.getElementById('root'));
+/* <UserElement user="Raihan" locale="bn-BD" /> */
+
+ReactDom.render(<UserElement locale="bn-BD">Raihan</UserElement>, document.getElementById('root'));
